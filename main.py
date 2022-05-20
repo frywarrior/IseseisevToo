2eec7db2a4128fdf7dfc52e32bed411fec7f7f6e
frywarrior <fkikkas@gmail.com>
2022-05-20 10:04:15 +0300

    yah

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import pygame, sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+
+# ekraani seaded
+screenX = 640
+screenY = 480
+screen = pygame.display.set_mode([screenX, screenY])
+pygame.display.set_caption("Animeerimine")
+clock = pygame.time.Clock()
+
+# graafika laadimine
+bg = pygame.image.load("img/bg_rally.jpg")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+f1_blue = pygame.image.load("img/f1_blue.png")
+f1_blue = pygame.transform.rotate(f1_blue, 180)
 
+f1_red = pygame.image.load("img/f1_red.png")
+f1_red = pygame.transform.rotate(f1_red, 180)
 
-# Press the green button in the gutter to run the script..
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# kiirus ja asukoht
+BposX, BposY = 420, 0
+BspeedY = 3
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+RposX, RposY = 298, 0
+RspeedY = 1
+gameover = False
+while not gameover:
+    # fps
+    clock.tick(60)
+    # mängu sulgemine ristist
+    events = pygame.event.get()
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            quit()
+            sys.exit()
+
+    # pildi lisamine ekraanile
+    screen.blit(bg, (0, 0))
+
+    screen.blit(f1_blue, (BposX, BposY))
+    BposY += BspeedY
+
+    screen.blit(f1_red, (RposX, RposY))
+    RposY += RspeedY
+
+    if BposY >= screenY:
+        BposY = -120
+
+    # graafika kuvamine ekraanil
+    pygame.display.flip()
