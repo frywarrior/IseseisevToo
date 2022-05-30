d2b24d2f558b8e3fa145638ebd8ed9fc6086f79b
frywarrior <fkikkas@gmail.com>
2022-05-30 19:41:04 +0300

    yas

--- Yl7/Ringid.py
+++ Yl7/Ringid.py
@@ -1,11 +1,11 @@
-import pygame, random
-screen, im = pygame.display.set_mode([640, 480]), 8
-screen.fill([0, 0, 0])
-while True:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            quit()
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.circle(screen, [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)], pygame.mouse.get_pos(), im, 1)
-            im += 4
-    pygame.display.flip()
+import pygame, random  # impordib pygame ja random'i
+screen, im = pygame.display.set_mode([640, 480]), 8  # loob ekraani suurusega 640 x 480 ja ringi suuruse
+screen.fill([0, 0, 0])  # täidabe kraani mustaga
+while True:  # kui on tõene
+    for i in pygame.event.get():  # võtab evendid
+        if i.type == pygame.QUIT:  # kui kasutaja vajutab "X" nupule
+            quit()  # väljub mängust
+        if i.type == pygame.MOUSEBUTTONDOWN:  # kui hiirenupp on all
+            pygame.draw.circle(screen, [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)],pygame.mouse.get_pos(), im, 1)  # joonistab ringi
+            im += 4  # Uuendab järgmise ringi suurust
+    pygame.display.flip()  # uuendab ekraani
