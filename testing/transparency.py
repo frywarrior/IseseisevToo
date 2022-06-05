1aef4fa89c92f7c7e86359770847cdfc7a3d4dff
frywarrior <fkikkas@gmail.com>
2022-06-05 14:33:11 +0300

    test

---
+++ testing/transparency.py
@@ -0,0 +1,6 @@
+"""
+s = pygame.Surface((200, 300))  # the size of your rect
+s.set_alpha(200)  # alpha level
+s.fill((0, 0, 50))  # this fills the entire surface
+background.blit(s, (0, 0))  # (0,0) are the top-left coordinates
+"""
