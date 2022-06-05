b0521d201dc00f160b765bcaa8c8317c0c1af5e2
frywarrior <fkikkas@gmail.com>
2022-06-05 18:22:21 +0300

    reformat main, yas

--- Ns/main.py
+++ Ns/main.py
@@ -1,81 +1,82 @@
 # Franco Kikkas IS21, Lis ns kuna igav
 import pygame
 from world import Hero
-from pub import Pub
+from pub import Pub as Pubd
 from Escmenu import escmenu
 
+def main():
+    def draw():
+        frame = pygame.transform.scale(screen, (screenX, screenY))
+        window.blit(frame, frame.get_rect())
+        pygame.display.flip()
 
-def draw():
-    frame = pygame.transform.scale(screen, (screenX, screenY))
-    window.blit(frame, frame.get_rect())
-    pygame.display.flip()
+    def loop():
+        # fps
+        clock.tick(30)
+        # mängu sulgemine ristist
+        events = pygame.event.get()
+        for i in events:
+            if i.type == pygame.QUIT:
+                quit()
+            if i.type == pygame.MOUSEBUTTONDOWN:
+                pos = list(pygame.mouse.get_pos())
+                # take the mouse position and scale it, too
+                ratio_x = (window_rect.width / screen_rect.width)
+                ratio_y = (window_rect.height / screen_rect.height)
+                scaled_pos = ((pos[0] / ratio_x) - hero.sc_x, (pos[1] / ratio_y) - hero.sc_y)
 
+                print(scaled_pos)
+                print(pygame.mouse.get_pos())
+            if i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_ESCAPE:
+                    escmenu(window)
 
-def loop():
-    # fps
-    clock.tick(30)
-    # mängu sulgemine ristist
-    events = pygame.event.get()
-    for i in events:
-        if i.type == pygame.QUIT:
-            quit()
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            pos = list(pygame.mouse.get_pos())
-            # take the mouse position and scale it, too
-            ratio_x = (window_rect.width / screen_rect.width)
-            ratio_y = (window_rect.height / screen_rect.height)
-            scaled_pos = ((pos[0] / ratio_x) - hero.sc_x, (pos[1] / ratio_y) - hero.sc_y)
+    pygame.init()
 
-            print(scaled_pos)
-            print(pygame.mouse.get_pos())
-        if i.type == pygame.KEYDOWN:
-            if i.key == pygame.K_ESCAPE:
-                escmenu(window)
+    # ekraani seaded
+    screenX = 640
+    screenY = 480
+    screenPosX, screenPosY = -200, -450
 
+    window = pygame.display.set_mode([screenX, screenY])
+    window_rect = window.get_rect()
 
-pygame.init()
+    screen = pygame.Surface([160, 144])
+    screen_rect = screen.get_rect()
 
-# ekraani seaded
-screenX = 640
-screenY = 480
-screenPosX, screenPosY = -200, -450
+    pygame.display.set_caption("Game_Test")
+    clock = pygame.time.Clock()
+    #
 
-window = pygame.display.set_mode([screenX, screenY])
-window_rect = window.get_rect()
+    # graafika laadimine
 
-screen = pygame.Surface([160, 144])
-screen_rect = screen.get_rect()
+    hero = Hero(screen, (72, 66), (screenPosX, screenPosY))
+    Pub = Pubd(screen, (72, 66), (-21, -60))
 
-pygame.display.set_caption("Game_Test")
-clock = pygame.time.Clock()
-#
+    #
+    gameover = False
 
-# graafika laadimine
+    world = True
+    pub = False
+    while not gameover:
+        while world:
+            loop()
 
-hero = Hero(screen, (72, 66), (screenPosX, screenPosY))
-Pub = Pub(screen, (72, 66), (-21, -60))
+            hero.moving()
+            if hero.pub_collide():
+                world = False
+                pub = True
+                screen.fill([0, 0, 0])
+            draw()
 
-#
-gameover = False
+        while pub:
+            loop()
+            Pub.moving()
+            if Pub.pub_collide():
+                world = True
+                pub = False
 
-world = True
-pub = False
-while not gameover:
-    while world:
-        loop()
+            draw()
 
-        hero.moving()
-        if hero.pub_collide():
-            world = False
-            pub = True
-            screen.fill([0, 0, 0])
-        draw()
-
-    while pub:
-        loop()
-        Pub.moving()
-        if Pub.pub_collide():
-            world = True
-            pub = False
-
-        draw()
+if __name__ == "__main__":
+    main()
