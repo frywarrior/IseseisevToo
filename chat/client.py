620a28366a61cd4566e4513ad27105aa6613e831
franco.kikkas <fkikkas@gmail.com>
2022-06-07 10:06:27 +0300

    yah

--- chat/client.py
+++ chat/client.py
@@ -1,17 +1,17 @@
 import socket  # importib mooduli nimega socket
 
 s = socket.socket()  # loob uue pistikupesa# loob uue pistikupesa
-host = input(str("Please enter the hostname of the server:"))
+host = input(str("Please enter the hostname of the server:"))  # prindib teksti
 port = 8081  # seab ühenduse pordi
 s.connect((host, port))  # ühendab pistikupesa sellele aadressile
-print("Connected to chat server")
-while 1:
-    incoming_message = s.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Server:", incoming_message)
-    print("")
-    message = input(str(">>"))
-    message = message.encode()
-    s.send(message)
-    print("message has been sent ...")
-    print("")
+print("Connected to chat server")  # prindib teksti
+while 1:  # kui on tõene
+    incoming_message = s.recv(1024) # võtab socketist infot
+    incoming_message = incoming_message.decode() # dekodeerib saadud sõnumi
+    print("Server:", incoming_message) # prindib sõnumi
+    print("") # prindib tühja rea
+    message = input(str(">>"))  # võtab kasutaja poolt sisestatud sõnumi
+    message = message.encode() # kodeerib sõnumi
+    s.send(message) # saadab ühendusele eelneva sõnumi
+    print("message has been sent ...")  # prindib teksti
+    print("")   # prindib tühja rea
