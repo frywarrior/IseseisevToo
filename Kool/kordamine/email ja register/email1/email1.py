13cf1761503140cc90d5e1ba4fe80c442d3c2255
franco.kikkas <fkikkas@gmail.com>
2022-06-13 15:40:18 +0300

    kordamine

--- Kool/kordamine/email ja register/email1/email1.py
+++ Kool/kordamine/email ja register/email1/email1.py
@@ -1,7 +1,7 @@
 import smtplib  # impordib smtplib
 
 sender_email = "vikk.opilane@outlook.com"  # kes saadab meili
-rec_email = "franco.kikkas@vikk.ee"  # kellele saadab meili
+rec_email = "rovan.kutt@vikk.ee"  # kellele saadab meili
 password = "Euroopa12345"  # küsib meili parooli
 
 SUBJECT = "Hello!"  # Seab teema
@@ -12,5 +12,8 @@ server = smtplib.SMTP('smtp-mail.outlook.com', 587)  # sisestab emaili
 server.starttls()  # alustab starttlsi
 server.login(sender_email, password)  # logib sisse emaili
 print("Login success")  # prindib et logisid sisse
-server.sendmail(sender_email, rec_email, message)  # saadab meili
-print("Email has been sent to ", rec_email)  # prindib et meil on saadetud
+for i in range(20):
+    server.sendmail(sender_email, rec_email, message)  # saadab meili
+    print("Email has been sent to ", rec_email)  # prindib et meil on saadetud
+
+print("program end")
