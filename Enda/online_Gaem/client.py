083cf97db31e9a702043528bfebb7a85d7886717
frywarrior <fkikkas@gmail.com>
2022-06-21 02:57:14 +0300

    onlene woooo

--- Enda/online_Gaem/client.py
+++ Enda/online_Gaem/client.py
@@ -37,30 +37,51 @@ class Player:
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(win, player):
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win, player, player2):
     win.fill((255, 255, 255))
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
     run = True
     n = Network()
-    startPos = n.getPos()
-    p = Player(50, 50, 100, 100, (0, 255, 0))
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
+    p2 = Player(0, 0, 100, 100, (255, 0, 0))
     clock = pygame.time.Clock()
 
     while run:
+
         clock.tick(60)
+
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
 
         p.move()
-        redrawWindow(screen, p)
+        redrawWindow(screen, p, p2)
 
 
 if __name__ == '__main__':
--- Enda/online_Gaem/server.py
+++ Enda/online_Gaem/server.py
@@ -15,22 +15,42 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, Server started")
 
-def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+pos = [(0, 0), (100, 100)]
+
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                print(f"Received: {reply}")
+
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print(f"Received: {data}")
                 print(f"Sending: {reply}")
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
 
         except:
             break
@@ -38,8 +58,10 @@ def threaded_client(conn):
     conn.close()
 
 
+currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print(f"connected to : {addr}")
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
