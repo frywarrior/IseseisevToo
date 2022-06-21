452049435e1d51bef8af2c431a6d0cca14306d06
franco.kikkas <fkikkas@gmail.com>
2022-06-21 10:21:17 +0300

    network, yaaas

--- Enda/online_Gaem/server.py
+++ Enda/online_Gaem/server.py
@@ -2,11 +2,11 @@ import socket
 from _thread import *
 import sys
 
-server = "0.0.0.0"
-port = 10002
+server = "10.35.140.130"
+port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# listen on all interfaces
+
 s.bind((server, port))
 
 s.listen(2)
