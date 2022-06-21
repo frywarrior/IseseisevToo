d328e916e5596145a75e265c8065ab018c737d9a
frywarrior <fkikkas@gmail.com>
2022-06-21 19:10:27 +0300

    network

--- Enda/online_Gaem/network.py
+++ Enda/online_Gaem/network.py
@@ -4,7 +4,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.35.140.130"
+        self.server = "10.10.10.156"
         self.port = 10002
         self.addr = (self.server, self.port)
         self.pos = self.connect()
